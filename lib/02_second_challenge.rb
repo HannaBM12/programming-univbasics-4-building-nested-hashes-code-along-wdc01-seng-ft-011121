3c3f9812a349864dbdf7251aa93bfbc6bc3fb643
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-12-29 01:33:12 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -1,29 +1,16 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {:patriarch => {
-                      :name => "Lord Montague",
-                      :age => "53"
-                            },
-                  :matriarch => {
-                      :name => "Lady Montague",
-                      :age => "54"
-                            },
-                  :hero => {
-                    :name => "Romeo",
-                    :age => "15"
-                            },
-                  :hero_friends => {
-                    :name => "Benvolio",
-                    :age => "17",
-                    :attitude => "worried"
-                    }
+    :montague => {:patriarch => {},
+                  :matriarch => {},
+                  :hero => {},
+                  :hero_friends => {}
                   },
     :capulet => {
       #your key/value pairs here!
-                    :patriarch =>{},
-                    :matriarch =>{},
+                    :patriarch => {},
+                    :matriarch => {},
                     :heroine => {},
-                    :heroine_friends =>{}
+                    :heroine_friends => {}
       }}
 
 
