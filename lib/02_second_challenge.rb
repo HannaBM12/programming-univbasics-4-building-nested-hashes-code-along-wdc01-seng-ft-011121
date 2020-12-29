742a5b49520b276d8e89e13c61218b67ba348d71
Hanna Mulugeta <hannabmulugeta@gmail.com>
2020-12-29 01:22:31 +0000

    Automatically backed up by Learn

--- lib/02_second_challenge.rb
+++ lib/02_second_challenge.rb
@@ -5,7 +5,7 @@ def second_challenge
                   :hero => {},
                   :hero_friends => {}
       #your key/value pairs here!
-      }, 
+      },
     :capulet => {
       #your key/value pairs here!
                     :patriarch => {},
@@ -14,5 +14,5 @@ def second_challenge
                     :heroine_friends => {}
       }}
 
-  
+
 end
